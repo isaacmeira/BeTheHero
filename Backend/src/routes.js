eb6dde75cfb36a74e1e6b513e9b921aefbb9873f
isaac meira <fspp@outlook.com>
2020-03-25 00:19:17 -0300

    finished the backend

---
+++ Backend/src/routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+routes.post('/ongs', OngController.create);
+routes.get('/ongs', OngController.list);
+
+routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.list);
+routes.delete('/incidents/:id', IncidentController.delete)
+
+routes.get('/profile', ProfileController.list)
+
+routes.post('/sessions', SessionController.create);
+
+module.exports = routes;
